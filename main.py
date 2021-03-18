d4491836448ba3be714e70fb21eaa9503620e910
FLT4613 <ft4613st@gmail.com>
2021-03-18 19:03:55 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import csv
 import xml.etree.ElementTree as ET
-from collections import defaultdict
 
 if __name__ == "__main__":
     status_strs = [
@@ -11,24 +10,17 @@ if __name__ == "__main__":
         "Everything works",
     ]
 
-    with open('modlist.csv', encoding='utf8') as f:
-        modlist = {
-            x[1]: (x[0], x[1])
-            for x in csv.reader(f)
-        }
+    with open("modlist.csv", encoding="utf8") as f:
+        modlist = {x[1]: (x[0], x[1]) for x in csv.reader(f)}
 
-    with open('modlist.xml', encoding='utf8') as f:
+    with open("modlist.xml", encoding="utf8") as f:
         xml_str = f.read()
 
     xml = ET.fromstring(xml_str)
-    mods_status = sorted([
-        modlist[id.text] for id in xml.findall('./modNames/li')
-        if id.text in modlist
-    ], key=lambda x: x[0])
+    mods_status = sorted(
+        [modlist[id.text] for id in xml.findall("./modNames/li") if id.text in modlist], key=lambda x: x[0]
+    )
 
     for index, status_str in enumerate(status_strs):
-        print(f'[{index}: {status_str}]')
-        print(
-            '\n'.join([f'\t{x[1]}' for x in mods_status if x[0] == str(index)])
-        )
-        print('\n')
+        print(f"[{index}: {status_str}]")
+        print("".join([f"\t{x[1]}\n" for x in mods_status if x[0] == str(index)]))
